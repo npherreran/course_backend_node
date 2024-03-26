ef308e71fdee4357ea5d7ca19b90efbf56e0512e
Natalia Herrera Neira <la.nataliaherrera@MacBook-Pro-de-Natalia.local>
2024-03-26 11:28:38 -0300

    FIX: port issue fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000 
 const format = require('date-format')
 
 app.listen(PORT, () => {
